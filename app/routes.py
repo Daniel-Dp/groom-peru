e94b1fcb49cedf5ee7f245ca0ca2a29904cdc8ce
Daniel-Dp <daniantoni_10@outlook.es>
2026-06-15 20:50:36 -0500

    maps de google

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_from_directory, current_app
+import os
 
 main_bp = Blueprint('main', __name__)
 
@@ -10,3 +11,12 @@ def index():
 @main_bp.route('/dermatologia')
 def dermatologia():
     return render_template('dermatologia.html')
+
+@main_bp.route('/sitemap.xml')
+def sitemap():
+    """Ruta para servir el sitemap desde la carpeta static."""
+    return send_from_directory(
+        os.path.join(current_app.root_path, 'static'), 
+        'sitemap.xml', 
+        mimetype='application/xml'
+    )
